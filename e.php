c50c5961d0802e7460953e22e4ccaebbde451865
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 00:14:13 +0100

    '12'

--- e.php
+++ e.php
@@ -80,6 +80,7 @@ if ($result->num_rows > 0) {
 							$conn->query($sql);
 							*/
 						}else{
+							echo date('w',$orain[$i+1])." ".date('w',$orafin[$i])."<br>";
 							$sql = "INSERT INTO freeaula (aula, polo, orainizio, orafine)
 								VALUES ('".$row["aula"]."','".$row["polo"]."',".$orafin[$i].",".$orain[$i+1].")";
 							$conn->query($sql);
