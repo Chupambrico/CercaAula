af2f50d40d6fbcb85b7fa7ec4bc62be7a51051e5
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 23:01:18 +0100

    '13'

--- design/products/test/index.php
+++ design/products/test/index.php
@@ -74,15 +74,15 @@
 					<a href="#" class="deploy-sidebar"></a>
 				</div>
 				<div class="container spazio-fondo center-text">
-					<span class="custom-dropdown custom-dropdown--blue">
+					<span class="custom-dropdown--small custom-dropdown--blue">
 						<select class="custom-dropdown__select custom-dropdown__select--blue day">
 						</select>
 					</span>
-					<span class="custom-dropdown custom-dropdown--blue">
+					<span class="custom-dropdown--small custom-dropdown--blue">
 						<select class="custom-dropdown__select custom-dropdown__select--blue orainizio">
 						</select>
 					</span>
-					<span class="custom-dropdown custom-dropdown--blue">
+					<span class="custom-dropdown--small custom-dropdown--blue">
 						<select class="custom-dropdown__select custom-dropdown__select--blue orafine">
 						</select>
 					</span>
