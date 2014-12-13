c539fe861270514fbd1a983ad216e9d564accc25
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 20:11:52 +0100

    '13'

--- design/products/test/index.php
+++ design/products/test/index.php
@@ -23,6 +23,7 @@
 <link href="styles/style.css"				rel="stylesheet" type="text/css">
 <link href="styles/framework-style.css" 	rel="stylesheet" type="text/css">
 <link href="styles/framework.css" 			rel="stylesheet" type="text/css">
+<link href="styles/bxslider.css"			rel="stylesheet" type="text/css">
 <link href="styles/swipebox.css"			rel="stylesheet" type="text/css">
 <link href="styles/icons.css"				rel="stylesheet" type="text/css">
 <link href="styles/retina.css" 				rel="stylesheet" type="text/css" media="only screen and (-webkit-min-device-pixel-ratio: 2)" />
@@ -37,6 +38,7 @@
 <script src="scripts/subscribe.js"		 type="text/javascript"></script>
 <script src="scripts/contact.js"		 type="text/javascript"></script>
 <script src="scripts/jquery.swipebox.js" type="text/javascript"></script>
+<script src="scripts/bxslider.js"		 type="text/javascript"></script>
 <script src="scripts/colorbox.js"		 type="text/javascript"></script>
 <script src="scripts/retina.js"			 type="text/javascript"></script>
 <script src="scripts/custom.js"			 type="text/javascript"></script>
@@ -131,64 +133,6 @@
         <div class="container no-bottom">
         	<h4 class="heading center-text">FROM THE GALLERY</h4>
             <p class="center-text">We've included a swipe gallery, both on the homepage and on a dedicated page! Try it out!</p>
-        </div>
-        
-        <div class="container-gray">
-            <ul class="gallery gallery-homepage">
-                <li>
-                    <a class="swipebox" href="images/gallery/full/1.jpg" title="An awesome gallery!">
-                        <img src="images/slider/0s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/2.jpg" title="An awesome gallery!">
-                        <img src="images/slider/1s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/3.jpg" title="An awesome gallery!">
-                        <img src="images/slider/2s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/4.jpg" title="An awesome gallery!">
-                        <img src="images/slider/3s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/5.jpg" title="An awesome gallery!">
-                        <img src="images/slider/4s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/2.jpg" title="An awesome gallery!">
-                        <img src="images/slider/3s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/1.jpg" title="An awesome gallery!">
-                        <img src="images/slider/0s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/2.jpg" title="An awesome gallery!">
-                        <img src="images/slider/1s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-                <li>
-                    <a class="swipebox" href="images/gallery/full/3.jpg" title="An awesome gallery!">
-                        <img src="images/slider/2s.jpg" alt="Image 001" />
-                    </a>
-                </li>
-            </ul> 
-            <div class="column no-bottom">
-                <div class="one-half">
-                    <a href="#" class="flat-button flat-gallery">OUR GALLERY</a>
-                </div>
-                <div class="one-half">
-                    <a href="#" class="flat-button flat-portfolio">OUR PORTFOLIO</a>
-                </div>
-            </div>
         </div>
         
         <div class="container no-bottom">
