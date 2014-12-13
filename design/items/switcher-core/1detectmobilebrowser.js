4e7a834455dcc91e50ddb9c1da241767583053a7
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 01:36:10 +0100

    '12'

---
+++ design/items/switcher-core/1detectmobilebrowser.js
@@ -0,0 +1,283 @@
+if (window.location.search == '?theme=gliders') {
+	var stringer = 'http://www.enableds.com/products/gliders/';
+};
+
+if (window.location.search == '?theme=slideout') {
+	var stringer = 'http://www.enableds.com/products/slideout/';
+};
+
+if (window.location.search == '?theme=boxed') {
+	var stringer = 'http://www.enableds.com/products/boxed/';
+};
+
+if (window.location.search == '?theme=glovebox') {
+	var stringer = 'http://www.enableds.com/products/glovebox/';
+};
+
+if (window.location.search == '?theme=profiler') {
+	var stringer = 'http://www.enableds.com/products/oceam/';
+};
+
+if (window.location.search == '?theme=profiler') {
+	var stringer = 'http://www.enableds.com/products/profiler/';
+};
+
+if (window.location.search == '?theme=blureh') {
+	var stringer = 'http://www.enableds.com/products/blureh/';
+};
+
+if (window.location.search == '?theme=trolley') {
+	var stringer = 'http://www.enableds.com/products/trolley/dark/';
+};
+
+if (window.location.search == '?theme=sandglass') {
+	var stringer = 'http://www.enableds.com/products/sandglass/';
+};
+
+if (window.location.search == '?theme=reaves') {
+	var stringer = 'http://www.enableds.com/products/reaves/unboxed/';
+};
+
+if (window.location.search == '?theme=anechoic') {
+	var stringer = 'http://www.enableds.com/products/anechoic/index-regular-slider.html';
+};
+
+if (window.location.search == '?theme=scrolled') {
+	var stringer = 'http://www.enableds.com/products/scrolled/';
+};
+
+
+if (window.location.search == '?theme=slasher') {
+	var stringer = 'http://www.enableds.com/products/slasher/';
+};
+
+if (window.location.search == '?theme=ciuter') {
+	var stringer = 'http://www.enableds.com/products/ciuter/';
+};
+
+if (window.location.search == '?theme=stroller') {
+	var stringer = 'http://www.enableds.com/products/stroller/';
+};
+
+if (window.location.search == '?theme=sitebar') {
+	var stringer = 'http://www.enableds.com/products/sitebar/';
+};
+
+if (window.location.search == '?theme=slideby') {
+	var stringer = 'http://www.enableds.com/products/slideby/';
+};
+
+if (window.location.search == '?theme=photroller') {
+	var stringer = 'http://www.enableds.com/products/photroller/';
+};
+
+if (window.location.search == '?theme=faller') {
+	var stringer = 'http://www.enableds.com/products/faller/';
+};
+
+if (window.location.search == '?theme=hexagone') {
+	var stringer = 'http://www.enableds.com/products/hexagone/';
+};
+
+if (window.location.search == '?theme=chaliswp') {
+	var stringer = 'http://www.oxeso.com/chalis/';
+};
+
+if (window.location.search == '?theme=ezos') {
+	var stringer = 'http://www.enableds.com/products/ezos/';
+};
+
+if (window.location.search == '?theme=mobilebarwp') {
+	var stringer = 'http://www.oxeso.com/mobilebar/';
+};
+
+if (window.location.search == '?theme=chivalry') {
+	var stringer = 'http://www.enableds.com/products/chivalry/';
+};
+
+if (window.location.search == '?theme=swipebodywp') {
+	var stringer = 'http://www.oxeso.com/swipebody/';
+};
+
+if (window.location.search == '?theme=aufidius') {
+	var stringer = 'http://www.enableds.com/products/aufidius/';
+};
+
+if (window.location.search == '?theme=ciento') {
+	var stringer = 'http://www.enableds.com/products/ciento/';
+};
+
+if (window.location.search == '?theme=notifico') {
+	var stringer = 'http://www.enableds.com/products/notifico/';
+};
+
+if (window.location.search == '?theme=chalis') {
+	var stringer = 'http://www.enableds.com/products/chalis/';
+};
+
+if (window.location.search == '?theme=mobilebar') {
+	var stringer = 'http://www.enableds.com/products/mobilebar/';
+};
+
+if (window.location.search == '?theme=swipebody') {
+	var stringer = 'http://www.enableds.com/products/swipebody/';
+};
+
+if (window.location.search == '?theme=flaty') {
+	var stringer = 'http://www.enableds.com/products/flaty/';
+};
+
+
+if (window.location.search == '?theme=duodrawerwp') {
+	var stringer = 'http://www.oxeso.com/duodrawer/';
+};
+
+
+if (window.location.search == '?theme=mobiler') {
+	var stringer = 'http://www.enableds.com/products/mobiler/black/';
+};
+
+if (window.location.search == '?theme=handee') {
+	var stringer = 'http://www.enableds.com/products/handee/left/';
+};
+
+if (window.location.search == '?theme=drawerwp') {
+	var stringer = 'http://paultrifa.com/envato/themeforest/drawerwp/';
+};
+
+if (window.location.search == '?theme=webby') {
+	var stringer = 'http://www.enableds.com/products/webby/';
+};
+
+
+if (window.location.search == '?theme=modernerwp') {
+	var stringer = 'http://www.oxeso.com/moderner/';
+};
+
+
+if (window.location.search == '?theme=instanav') {
+	var stringer = 'http://www.enableds.com/products/instanav/';
+};
+
+if (window.location.search == '?theme=duodrawer') {
+	var stringer = 'http://www.enableds.com/products/duodrawer/';
+};
+
+
+if (window.location.search == '?theme=reside') {
+	var stringer = 'http://www.enableds.com/products/reside/';
+};
+
+if (window.location.search == '?theme=boldr') {
+	var stringer = 'http://www.enableds.com/products/boldr/';
+};
+
+if (window.location.search == '?theme=rsepina') {
+	var stringer = 'http://www.enableds.com/products/rsepina/';
+};
+
+if (window.location.search == '?theme=drawer') {
+	var stringer = 'http://www.enableds.com/products/drawer/fixed/';
+};
+
+if (window.location.search == '?theme=moderner') {
+	var stringer = 'http://www.enableds.com/products/moderner/';
+};
+
+if (window.location.search == '?theme=trollerwp') {
+	var stringer = 'http://oxeso.com/troller/';
+};
+
+if (window.location.search == '?theme=themetro') {
+	var stringer = 'http://www.enableds.com/products/metro/blue/';
+};
+
+if (window.location.search == '?theme=trollerv2') {
+	var stringer = 'http://www.enableds.com/products/troller2';
+};
+
+if (window.location.search == '?theme=drohp') {
+	var stringer = 'http://www.enableds.com/products/drohp/';
+};
+
+if (window.location.search == '?theme=troller') {
+	var stringer = 'http://www.enableds.com/products/troller/orange/';
+};
+
+if (window.location.search == '?theme=noiser') {
+	var stringer = 'http://www.enableds.com/products/noiser/left/';
+};
+
+if (window.location.search == '?theme=baller') {
+	var stringer = 'http://www.enableds.com/products/baller/orange/';
+};
+
+if (window.location.search == '?theme=booki') {
+	var stringer = 'http://www.enableds.com/products/booki/';
+};
+
+if (window.location.search == '?theme=quattra') {
+	var stringer = 'http://www.enableds.com/products/quattra/red/';
+};
+
+if (window.location.search == '?theme=arrow') {
+	var stringer = 'http://www.enableds.com/products/arrow/';
+};
+
+if (window.location.search == '?theme=ceeveewp') {
+	var stringer = 'http://www.oxeso.com/ceevee/';
+};
+
+if (window.location.search == '?theme=ceevee') {
+	var stringer = 'http://www.enableds.com/products/ceevee/';
+};
+
+if (window.location.search == '?theme=photobile') {
+	var stringer = 'http://www.enableds.com/products/photobile/black/';
+};
+
+if (window.location.search == '?theme=sidewp') {
+	var stringer = 'http://www.oxeso.com/side/';
+};
+
+if (window.location.search == '?theme=side') {
+	var stringer = 'http://www.enableds.com/products/side/red/';
+};
+
+if (window.location.search == '?theme=imgro') {
+	var stringer = 'http://www.enableds.com/products/imgro/sixe/';
+};
+
+if (window.location.search == '?theme=kaius') {
+	var stringer = 'http://www.enableds.com/products/kaius/';
+};
+
+if (window.location.search == '?theme=corvina') {
+	var stringer = 'http://www.enableds.com/products/corvina/';
+};
+
+if (window.location.search == '?theme=fosile') {
+	var stringer = 'http://www.enableds.com/products/fosile/green/';
+};
+
+if (window.location.search == '?theme=tricky') {
+	var stringer = 'http://www.enableds.com/products/tricky/';
+};
+
+if (window.location.search == '?theme=signum') {
+	var stringer = 'http://www.enableds.com/products/signum/main/';
+};
+
+if (window.location.search == '?theme=abeeso') {
+	var stringer = 'http://www.enableds.com/products/abeeso/mobile/';
+};
+
+if (window.location.search == '?theme=esepina') {
+	var stringer = 'http://www.enableds.com/products/esepina/main/';
+};
+
+
+(function (a, b) {
+    if (/android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|ipad|android|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) window.location = b
+})(navigator.userAgent || navigator.vendor || window.opera, stringer);
+
