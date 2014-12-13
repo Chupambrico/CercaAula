5ce21816a0412e43581ef37331db01b3799782b3
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 13:08:12 +0100

    '13'

--- design/products/test/sidebar.php
+++ design/products/test/sidebar.php
@@ -6,14 +6,14 @@
 		</div>
 		
 		<p class="sidebar-separator">NAVIGATION</p>
-		<a class="nav-item home-nav" href="index.html">						HOME		<em class="<?php ($page='h') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item type-nav" href="type.html">						TYPOGRAPHY	<em class="<?php ($page='t') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item jquery-nav" href="jquer.html">					jQUERY		<em class="<?php ($page='j') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item other-nav" href="other.html">					OTHERS		<em class="<?php ($page='o') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item gallery-nav" href="gallery.html">				GALLERY		<em class="<?php ($page='g') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item portfolio-nav" href="portfolio.html">			PORTFOLIO	<em class="<?php ($page='p') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item videos-nav" href="videos.html">					VIDEOS		<em class="<?php ($page='v') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item contact-nav no-border" href="contact.html">		CONTACT		<em class="<?php ($page='c') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item home-nav" href="index.php">						HOME		<em class="<?php ($page='h') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item type-nav" href="type.php">						TYPOGRAPHY	<em class="<?php ($page='t') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item jquery-nav" href="jquer.php">					jQUERY		<em class="<?php ($page='j') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item other-nav" href="other.php">					OTHERS		<em class="<?php ($page='o') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item gallery-nav" href="gallery.php">				GALLERY		<em class="<?php ($page='g') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item portfolio-nav" href="portfolio.php">			PORTFOLIO	<em class="<?php ($page='p') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item videos-nav" href="videos.php">					VIDEOS		<em class="<?php ($page='v') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
+		<a class="nav-item contact-nav no-border" href="contact.php">		CONTACT		<em class="<?php ($page='c') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
 		<p class="sidebar-separator">GET SOCIAL</p>
 		<a class="nav-item contact-nav" href="mailto:mail@domain.com">								MAIL		<em class="nav-link"></em></a>
 		<a class="nav-item call-nav" href="tel:+1 234 567 890">										CALL US		<em class="nav-link"></em></a>
