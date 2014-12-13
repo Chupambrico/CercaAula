3c96d217b394d4603edac175613cca3802c0f051
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 23:15:00 +0100

    '13'

--- design/products/test/index.php
+++ design/products/test/index.php
@@ -67,30 +67,33 @@
 			<?php include 'sidebar.php';?>
 			   
 			<div id="content" class="page-content">
-				<div class="page-header">
-					<p class="logo location custom-location">
-						<img src="images/misc/logo.png" width="63">
-					</p>
-					<a href="#" class="deploy-sidebar"></a>
-				</div>
-				<div class="container spazio-fondo center-text">
-					<span class="custom-dropdown custom-dropdown--blue custom-dropdown--small">
-						<select class="custom-dropdown__select custom-dropdown__select--blue day">
-						</select>
-					</span>
-					<span class="custom-dropdown custom-dropdown--blue custom-dropdown--small">
-						<select class="custom-dropdown__select custom-dropdown__select--blue orainizio">
-						</select>
-					</span>
-					<span class="custom-dropdown custom-dropdown--blue custom-dropdown--small">
-						<select class="custom-dropdown__select custom-dropdown__select--blue orafine">
-						</select>
-					</span>
-				</div>
-				<div class='decoration'></div>
-				<div class="container result">
-					<ul>
-					</ul>
+				<div class="wrapper">
+					<div class="page-header">
+						<p class="logo location custom-location">
+							<img src="images/misc/logo.png" width="63">
+						</p>
+						<a href="#" class="deploy-sidebar"></a>
+					</div>
+					<div class="container spazio-fondo center-text">
+						<span class="custom-dropdown custom-dropdown--blue custom-dropdown--small">
+							<select class="custom-dropdown__select custom-dropdown__select--blue day">
+							</select>
+						</span>
+						<span class="custom-dropdown custom-dropdown--blue custom-dropdown--small">
+							<select class="custom-dropdown__select custom-dropdown__select--blue orainizio">
+							</select>
+						</span>
+						<span class="custom-dropdown custom-dropdown--blue custom-dropdown--small">
+							<select class="custom-dropdown__select custom-dropdown__select--blue orafine">
+							</select>
+						</span>
+					</div>
+					<div class='decoration'></div>
+					<div class="container result">
+						<ul>
+						</ul>
+					</div>
+					<div class='push'></div>
 				</div>
 				
 				<div class="footer">
