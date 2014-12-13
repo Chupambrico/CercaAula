752e83e58fd72bb777292523bbb7be627a7f5538
cercaclasse <cercaclasse@hotmail.com>
2014-12-13 13:19:19 +0100

    '13'

--- design/products/test/index.php
+++ design/products/test/index.php
@@ -60,30 +60,7 @@
 
 <div class="all-elements">
     
-    <div class="page-sidebar" id="sidebar">
-	<div class="page-sidebar-scroll">
-		<div class="sidebar-header">
-			<a class="delete-sidebar" href="#"></a>
-			<img width="63" alt="img" src="images/misc/logo.png" class="sidebar-logo">
-		</div>
-		
-		<p class="sidebar-separator">NAVIGATION</p>
-		<a class="nav-item home-nav" href="index.php">						HOME		<em class="<?php ($page='h') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item type-nav" href="type.php">						TYPOGRAPHY	<em class="<?php ($page='t') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item jquery-nav" href="jquer.php">					jQUERY		<em class="<?php ($page='j') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item other-nav" href="other.php">						OTHERS		<em class="<?php ($page='o') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item gallery-nav" href="gallery.php">					GALLERY		<em class="<?php ($page='g') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item portfolio-nav" href="portfolio.php">				PORTFOLIO	<em class="<?php ($page='p') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item videos-nav" href="videos.php">					VIDEOS		<em class="<?php ($page='v') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<a class="nav-item contact-nav no-border" href="contact.php">		CONTACT		<em class="<?php ($page='c') ? echo"nav-onit" : echo"nav-page" ?>"></em></a>
-		<p class="sidebar-separator">GET SOCIAL</p>
-		<a class="nav-item contact-nav" href="mailto:mail@domain.com">								MAIL		<em class="nav-link"></em></a>
-		<a class="nav-item call-nav" href="tel:+1 234 567 890">										CALL US		<em class="nav-link"></em></a>
-		<a class="nav-item text-nav" href="sms:+1 234 567 890">										TEXT US		<em class="nav-link"></em></a>
-		<a class="nav-item facebook-nav" href="http://www.facebook.com/enabled.labs">				FACEBOOK	<em class="nav-link"></em></a>
-		<a class="nav-item twitter-nav" href="https://twitter.com/iEnabled">						TWITTER		<em class="nav-link"></em></a>
-	</div>
-</div>
+    <?php include 'sidebar.php';?>
        
     <div id="content" class="page-content">
         <div class="page-header">
