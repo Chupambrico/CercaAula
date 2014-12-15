a3df053fb7f894b4f2a48145f8b99d2fc008eec4
cercaclasse <cercaclasse@hotmail.com>
2014-12-15 02:21:02 +0100

    '14'

--- design/products/test/sidebar.php
+++ design/products/test/sidebar.php
@@ -6,12 +6,12 @@
 		</div>
 		
 		<p class="sidebar-separator">POLI</p>
-		<a class="nav-item home-nav" href="#about">						Povo			<em class="<?php echo ($page=='p') ? "nav-onit" : "nav-page" ?>"></em></a>
-		<a class="nav-item home-nav" href="#about">						Mesiano			<em class="<?php echo ($page=='m') ? "nav-onit" : "nav-page" ?>"></em></a>
-		<a class="nav-item home-nav" href="#about">						Lettere			<em class="<?php echo ($page=='l') ? "nav-onit" : "nav-page" ?>"></em></a>
-		<a class="nav-item home-nav" href="#about">						Sociologia		<em class="<?php echo ($page=='s') ? "nav-onit" : "nav-page" ?>"></em></a>
-		<a class="nav-item home-nav" href="#about">						Giurisprudenza	<em class="<?php echo ($page=='g') ? "nav-onit" : "nav-page" ?>"></em></a>
-		<a class="nav-item home-nav" href="#about">						Psicologia		<em class="<?php echo ($page=='a') ? "nav-onit" : "nav-page" ?>"></em></a>
-		<a class="nav-item home-nav" href="#about">						CIBIO			<em class="<?php echo ($page=='c') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item povo-nav" href="#about">						Povo			<em class="<?php echo ($page=='p') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item mesiano-nav" href="#about">						Mesiano			<em class="<?php echo ($page=='m') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item lettere-nav" href="#about">						Lettere			<em class="<?php echo ($page=='l') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item sociologia-nav" href="#about">						Sociologia		<em class="<?php echo ($page=='s') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item giurisprudenza-nav" href="#about">						Giurisprudenza	<em class="<?php echo ($page=='g') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item psicologia-nav" href="#about">						Psicologia		<em class="<?php echo ($page=='a') ? "nav-onit" : "nav-page" ?>"></em></a>
+		<a class="nav-item cibio-nav" href="#about">						CIBIO			<em class="<?php echo ($page=='c') ? "nav-onit" : "nav-page" ?>"></em></a>
 	</div>
 </div>
