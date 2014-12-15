e1045fe9ce692b03093a1b7b3e05d3faf4d0eb9d
cercaclasse <cercaclasse@hotmail.com>
2014-12-15 02:04:31 +0100

    '14'

--- design/products/test/selectDesktop.php
+++ design/products/test/selectDesktop.php
@@ -1,13 +1,13 @@
 <div class="container spazio-fondo center-text">
-						<span class="custom-dropdown custom-dropdown--blue custom-dropdown--large">
+						<span class="custom-dropdown custom-dropdown--blue">
 							<select class="custom-dropdown__select custom-dropdown__select--blue day">
 							</select>
 						</span>
-						<span class="custom-dropdown custom-dropdown--blue custom-dropdown--large">
+						<span class="custom-dropdown custom-dropdown--blue">
 							<select class="custom-dropdown__select custom-dropdown__select--blue orainizio">
 							</select>
 						</span>
-						<span class="custom-dropdown custom-dropdown--blue custom-dropdown--large">
+						<span class="custom-dropdown custom-dropdown--blue">
 							<select class="custom-dropdown__select custom-dropdown__select--blue orafine">
 							</select>
 						</span>
