553d30d985898a126ceca6bfba6888c921369a5d
cercaclasse <cercaclasse@hotmail.com>
2014-12-15 16:42:20 +0100

    '15'

--- en.php
+++ en.php
@@ -34,7 +34,7 @@ if (mysqli_query($conn, $sql)) {
 			} else {
 				echo "Error: " . $sql . "<br>" . mysqli_error($conn)."<br>";
 			}
-$sql="SELECT aula, polo, orainizio, orafine, data INTO orariom FROM orario";
+$sql="INSERT INTO orariom SELECT * FROM orario"; 
 $conn->query($sql);
 if (mysqli_query($conn, $sql)) {
 				//echo "New record created successfully<br>";
