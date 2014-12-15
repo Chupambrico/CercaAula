e4efca67dfadeb8fddfea838c3b747c563a7fd30
cercaclasse <cercaclasse@hotmail.com>
2014-12-15 02:47:44 +0100

    '14'

--- design/products/test/selectDesktop.php
+++ design/products/test/selectDesktop.php
@@ -1,14 +1,14 @@
 <div class="container spazio-fondo center-text">
-						<span class="custom-dropdown custom-dropdown--blue"style="width:140px">
-							<select class="custom-dropdown__select custom-dropdown__select--blue day">
+						<span class="custom-dropdown custom-dropdown--blue" style="width:140px">
+							<select class="custom-dropdown__select custom-dropdown__select--blue day" style="width:100%">
 							</select>
 						</span>
-						<span class="custom-dropdown custom-dropdown--blue"style="width:140px">
-							<select class="custom-dropdown__select custom-dropdown__select--blue orainizio">
+						<span class="custom-dropdown custom-dropdown--blue" style="width:140px">
+							<select class="custom-dropdown__select custom-dropdown__select--blue orainizio" style="width:100%">
 							</select>
 						</span>
-						<span class="custom-dropdown custom-dropdown--blue"style="width:140px">
-							<select class="custom-dropdown__select custom-dropdown__select--blue orafine">
+						<span class="custom-dropdown custom-dropdown--blue" style="width:140px">
+							<select class="custom-dropdown__select custom-dropdown__select--blue orafine" style="width:100%">
 							</select>
 						</span>
 					</div>
